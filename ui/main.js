90f4e55362728a79fa7e295e09f9d8bb5d7abca3
RamyaManoharan <30370582+RamyaManoharan@users.noreply.github.com>
2017-08-20 19:05:33 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,19 @@
+var button=document.getElementById('counter');
+button.onclick=function()
+{
+   var r=XMLHttpRequest();
+   
+   r.onreadystatechange = function()
+   {
+       if(r.readyState==XMLHttpRequest.DONE){
+       if(r.readyStatus==200){
+           var counter=r.responseText;
+           var span=document.getElementById('count');
+           span.innerHTML=counter.toString();
+       }
+       }
+       
+   };
+   r.open('GET',"http://ramyamano15.imad.hasura-app.io/counter",true );
+   r.send(null);
+};
