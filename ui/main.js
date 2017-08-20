b4910673cbc1c48fc0e9179bcf1cfede6ef0d907
RamyaManoharan <30370582+RamyaManoharan@users.noreply.github.com>
2017-08-20 16:09:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,14 @@ console.log('Loaded!');
 var a = document.getElementById('con');
 a.innerHTML = "Back to HIII!!!";
 
+var b=document.getElementById('madi');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    b.style.marginLeft=marginLeft+'px';
+}
+
+b.onclick = function() {
+  var c = setInterval(moveRight,100);
+};
