9d641fb7f2f352946f7e9a0a853165c62000e248
RamyaManoharan <30370582+RamyaManoharan@users.noreply.github.com>
2017-08-20 16:52:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/aro.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'ar1.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'aro.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
